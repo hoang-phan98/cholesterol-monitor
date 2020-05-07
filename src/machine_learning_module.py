c042f0ac87b207f1dd5f80ef0567483589172f15
Jason-Marshall-git <64062518+Jason-Marshall-git@users.noreply.github.com>
2020-05-07 16:42:57 +1000

    Testing machine learning algorithms (WIP).

--- src/machine_learning_module.py
+++ src/machine_learning_module.py
@@ -1,2 +1,10 @@
-import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from src.fhir_module import CholesterolDataClient, FHIRClient
 
+
+def plot_cholesterol_levels():
+    cholesterol = CholesterolDataClient("https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/")
+    colesterol_x = cholesterol.get_patient_data()
