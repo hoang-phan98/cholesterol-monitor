9dd44ad422d5e6e0deb2044e589c3d6f5abe42f6
Jason-Marshall-git <64062518+Jason-Marshall-git@users.noreply.github.com>
2020-05-10 21:30:26 +1000

    Updating machine_learning_module to use linear regression (WIP)

--- src/machine_learning_module.py
+++ src/machine_learning_module.py
@@ -1,10 +1,47 @@
+from src.fhir_module import CholesterolDataClient
 import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
-from src.fhir_module import CholesterolDataClient, FHIRClient
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 
-def plot_cholesterol_levels():
-    cholesterol = CholesterolDataClient("https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/")
-    colesterol_x = cholesterol.get_patient_data()
+def get_cholesterol_values():
+
+    client = CholesterolDataClient("https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/")
+    practitioner_id = int(input("Enter Practitioner ID: "))
+    patients = client.get_patient_list(practitioner_id)
+
+    patient_data = []
+    patient_ids = []
+
+    for patient in range(len(patients)):
+        patient_ids.append(patients.get_patient_list()[patient][2])
+        print(str(patients.get_patient_list()[patient][2]))
+
+    for patient_id in patient_ids:
+        patient_data.append(client.get_patient_data(patient_id))
+
+    return patient_data
+
+
+def plot_cholesterol_values():
+
+    cholesterol_values = get_cholesterol_values()
+    y_axis = []
+    for y_points in range(min(cholesterol_values), max(cholesterol_values) + 1):
+        y_axis.append(y_points)
+
+    plt.scatter(cholesterol_values, y_axis)
+
+    # Using linear regression machine learning algorithm
+    x = cholesterol_values
+    y = y_axis
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=10)
+
+    clf = LinearRegression()
+    clf.fit(x_train, y_train)
+    clf.predict(x_test)
+    clf.score(x_test, y_test)
+
+
+if __name__ == '__main__':
+    plot_cholesterol_values()
