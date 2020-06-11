30f184ee7a255d87902e2037e1dc9752d94ea6b1
Jason-Marshall-git <64062518+Jason-Marshall-git@users.noreply.github.com>
2020-06-11 12:29:39 +1000

    Updating the graphical observer to notify the graphs if the data changes.

--- src/application.py
+++ src/application.py
@@ -26,7 +26,8 @@ class GraphicalMonitor(Observer, FigureCanvasTkAgg):
 
     def update(self):
         if self._subject is not None:  # If subject has not been specified, do nothing
-            self.get_tk_widget().grid()
+            return self
+            # self.get_tk_widget().grid()
 
 
 class CholesterolMonitorTreeview(Observer, ttk.Treeview):
@@ -69,11 +70,6 @@ class BloodPressureMonitorTreeview(Observer, ttk.Treeview):
                 current_patient = self._subject.select_patient(patient_name)
                 if is_new_blood_pressure_data(values, current_patient):  # Check if the new values are different
                     self.item(item, values=format_data(current_patient)[3:])  # update the display with new data
-
-
-class GraphicalMonitor(Observer, Figure):
-    def update(self):
-        return
 
 
 class App:
@@ -347,9 +343,8 @@ class App:
                     time.sleep(1)
 
                 self.highlight_patients()
-
-                # self.cholesterol_graph_data()
-                # self.blood_pressure_graph_data()
+                self.cholesterol_graph_data()
+                self.blood_pressure_graph_data()
 
                 # Sleep
                 time.sleep(self.update_interval)
@@ -485,10 +480,6 @@ class App:
                         patient_data.append(float(patient_cholesterol))
                         patient_name = values[0]
                         patients.append(patient_name)
-                    else:
-                        patient_data.append(0)
-                        patient_name = values[0]
-                        patients.append(patient_name)
                 X = patients
                 Y = patient_data
                 return X, Y
@@ -499,7 +490,7 @@ class App:
     def cholesterol_graph(self, event=None):
 
         graph_data = self.cholesterol_graph_data()
-        figure = Figure(figsize=(10, 5), dpi=100)
+        figure = Figure(figsize=(5, 5), dpi=100)
         subplot = figure.add_subplot(1, 1, 1)
         subplot.bar(graph_data[0], graph_data[1], bottom=0)
         subplot.set_title("Patient Cholesterol Data (mg/dL)")
@@ -510,7 +501,8 @@ class App:
         cholesterol_graph.title("Cholesterol Graph")
         self.cholesterol_graphical_monitor = GraphicalMonitor(figure, master=cholesterol_graph)
         self.practitioner.get_monitored_patients().attach(self.cholesterol_graphical_monitor)
-        self.practitioner.get_monitored_patients().notify()
+        self.cholesterol_graphical_monitor.get_tk_widget().grid()
+        # self.practitioner.get_monitored_patients().notify()
         # canvas = FigureCanvasTkAgg(figure, master=cholesterol_graph)
         # canvas.get_tk_widget().grid()
 
@@ -568,9 +560,9 @@ class App:
         blood_pressure_graphs.title("Blood Pressure Graphs")
         self.BP_graphical_monitor = GraphicalMonitor(figure, master=blood_pressure_graphs)
         self.practitioner.get_monitored_patients().attach(self.BP_graphical_monitor)
-        self.practitioner.get_monitored_patients().notify()
+        self.BP_graphical_monitor.get_tk_widget().grid()
+        # self.practitioner.get_monitored_patients().notify()
         # canvas = FigureCanvasTkAgg(figure, master=blood_pressure_graphs)
-        # canvas.get_tk_widget().grid()
 
     def monitor_blood_pressure(self, event=None):
         """
